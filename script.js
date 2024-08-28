bc846c3f3a2f09802dde2e5fee3515d42ed22bed
felipecasal1 <felipecasalcornejo@gmail.com>
2024-08-27 21:36:02 -0300

    proyecto terminado

--- script.js
+++ script.js
@@ -3,36 +3,47 @@ let circlesFeedback = document.querySelectorAll(".desing-circle")
 
 let circleArray = [...circlesFeedback]
 
-circleArray.forEach(  boton =>{
 
+let pFeedBack = document.querySelector("#text-feedback")
+console.log(pFeedBack)
+
+
+circleArray.forEach(  boton =>{
+    
     
     boton.addEventListener("click", circle=>{
+        let numberFeedBack  = circle.target.textContent
         circleArray.forEach( btn => btn.classList.remove("active"))
     
-        let numberFeedBack  = circle.target.textContent
         console.log(numberFeedBack)
         let number  = circle.target
         console.log(number)
 
         number.classList.add("active")
+
+        showHide(numberFeedBack)
     })
 })
 
+function showHide(ele) {    
+    let numberSelected = ele
+    btnSubmit.addEventListener("click", () =>{
+    
+        if (numberSelected  > 0) {
+            firstModal.style.display = 'none';
+            secondModal.style.display = 'block';
+            pFeedBack.innerText = ` you selected ${numberSelected} of 5`
+        } else {
+            alert("Please select a score before submitting.");
+        }
+    })
 
-
-let firstModal = document.querySelector(".style-container")
-let secondModal = document.querySelector("#modal")
-
-
-console.log(firstModal)
-
-function showHide (){
-
-        if(firstModal.style.visibility == "hidden")
-        firstModal.style.visibility == "visible"
-    else{
-        firstModal.style.visibility == "hidden"
-    }
-    console.log(firstModal.style)
-    console.log("se reproduijo")
 }
+    
+    
+    
+    let firstModal = document.querySelector("#feedback-section")
+    let secondModal = document.querySelector("#modal")
+    console.log(firstModal)
+    
+    
